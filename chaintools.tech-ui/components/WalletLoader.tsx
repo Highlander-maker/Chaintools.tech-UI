1ddda47236069c9be0f90e9ccbc37bd206d91e58
CamelJuno <kloneb1995@yahoo.com>
2022-06-09 14:18:30 +0200

    Removed WalletLoader children and added page links to services and education buttons

--- chaintools.tech-ui/components/WalletLoader.tsx
+++ chaintools.tech-ui/components/WalletLoader.tsx
@@ -25,11 +25,12 @@ function WalletLoader({
   if (walletAddress !== '') {
     return (
       <div className="max-w-full">
+        <h1 className="text-6xl font-bold">Select network</h1>
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
           <Link href="https://explorer.chaintools.tech/" passHref>
             <a className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
-              <h3 className="text-2xl font-bold">Select Network &rarr;</h3>
-              <p className="mt-4 text-xl">Choose a network and stake your tokens to earn APR yield.</p>
+              <h3 className="text-2xl font-bold">Stake &rarr;</h3>
+              <p className="mt-4 text-xl">Stake you tokens to earn APR yield.</p>
             </a>
           </Link>
         </div>
@@ -49,7 +50,9 @@ function WalletLoader({
             Keplr wallet
           </a>
         </p> */}
-
+        <h1 className="text-6xl font-bold">
+          Welcome to Chaintools.tech
+        </h1>
         <div className="flex flex-wrap items-center justify-around md:max-w-4xl mt-6 sm:w-full">
           <button
             className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus"
@@ -57,36 +60,28 @@ function WalletLoader({
           >
             <h3 className="text-2xl font-bold">Stake to our Validators &rarr;</h3>
             <p className="mt-4 text-xl">
-              Connect your wallet to stake to Cosmos chains
+              Connect your wallet to stake to Cosmos chains.
             </p>
           </button>
         </div>
 
         <div className="flex flex-wrap items-center justify-around md:max-w-4xl mt-6 sm:w-full">
-          <button
-            className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus"
-            //onClick={connectWallet}
-          >
-            <h3 className="text-2xl font-bold">Services &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Relayers, statesync, snapshots, live peer updates
-            </p>
-          </button>
+          <Link href="/services" passHref>
+            <a className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
+              <h3 className="text-2xl font-bold">Services &rarr;</h3>
+              <p className="mt-4 text-xl">Relayers, statesync, snapshots, live peer updates.</p>
+            </a>
+          </Link>
         </div>
 
         <div className="flex flex-wrap items-center justify-around md:max-w-4xl mt-6 sm:w-full">
-          <button
-            className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus"
-            //onClick={connectWallet}
-          >
-            <h3 className="text-2xl font-bold"> Education &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Get to know how to validate, whats involved with infrastructure
-            </p>
-          </button>
+          <Link href="/education" passHref>
+            <a className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
+              <h3 className="text-2xl font-bold">Education &rarr;</h3>
+              <p className="mt-4 text-xl">Get to know how to validate, whats involved with infrastructure.</p>
+            </a>
+          </Link>
         </div>
-
-
       </div>
     )
   }
--- chaintools.tech-ui/pages/index.tsx
+++ chaintools.tech-ui/pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
 import WalletLoader from 'components/WalletLoader'
-import WelcomeLoader from 'components/WelcomeLoader'
 
 const Home: NextPage = () => {
 
   return (
     <>
-    <WelcomeLoader/>
     <WalletLoader/>
     </>
   )
